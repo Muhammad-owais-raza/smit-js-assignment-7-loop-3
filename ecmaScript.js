55a5d7faadc06c47a52573f1765467383875f9c9
Muhammad-owais-raza <owaisraza.codes@gmail.com>
2026-07-12 14:06:51 +0500

    complet

--- ecmaScript.js
+++ ecmaScript.js
@@ -1,11 +1,21 @@
 
 
-const Fruit = ["mango", "apple", "watermelon", "apple", "grape", "Apple"];
+const fruit = ["mango", "apple", "watermelon", "apple", "grape", "Apple"];
+
+let fruitName = prompt("Enter fruit name");
+
+let count = 0;
 
 
 
-for (let i =0; i <= 5; i++) {
+for (let i =0; i < fruit.length; i++) {
     
-    console.log(Fruit[i]);
+    if(fruit[i].toLowerCase() === fruitName.toLowerCase()){
+        count++;
+        
+    }
 }
+
+console.log(count);
+
 
